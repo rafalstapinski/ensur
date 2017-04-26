c4c5f93b42eb95ba39c301de0049a1110fb05b61
Rafal Stapinski <stapinskirafal@gmail.com>
2017-04-25 23:17:44 -0400

    message send done

--- node/node.py
+++ node/node.py
@@ -41,6 +41,80 @@ class user_new:
 
         return write({'message': 'error creating user'}, 500)
 
+class user_get:
+    def POST(self):
+
+        new_request(self)
+
+        i = web.input()
+
+        try:
+            username = i['username'].encode('utf-8')
+        except KeyError:
+            return write({'message': 'you must provide username and pubkey'}, 400)
+        except UnicodeError:
+            return write({'message': 'inputs must be utf-8 encoded'}, 400)
+        except:
+            return write({'message': 'something went wrong'}, 500)
+
+        dbvars = dict(username=username)
+
+        results = db.conn.select('users', dbvars, where='username = $username')
+
+        for result in results:
+            return write({'pubkey': result['pubkey']}, 200)
+
+        return write({'message': 'User does not exist. '}, 404)
+
+class message_new:
+    def POST(self):
+
+        new_request(self)
+
+        i = web.input()
+
+        try:
+            receiver = i['receiver'].encode('utf-8')
+            message = i['message'].encode('utf-8')
+            sender = i['sender'].encode('utf-8')
+        except KeyError:
+            return write({'message': 'you must provide username and pubkey'}, 400)
+        except UnicodeError:
+            return write({'message': 'inputs must be utf-8 encoded'}, 400)
+        except:
+            return write({'message': 'something went wrong'}, 500)
+
+        seq_id = db.conn.insert('messages', receiver=receiver, sender=sender, message=message)
+
+        if seq_id is not None:
+            return write({'message': 'message sent'}, 200)
+
+        return write({'message': 'error sending message'}, 500)
+
+class message_get:
+
+    def POST(self):
+
+        new_request(self)
+
+        i = web.input()
+
+        try:
+            receiver = i['receiver'].encode('utf-8')
+            sender = i['sender'].encode('utf-8')
+        except KeyError:
+            return write({'message': 'you must provide username and pubkey'}, 400)
+        except UnicodeError:
+            return write({'message': 'inputs must be utf-8 encoded'}, 400)
+        except:
+            return write({'message': 'something went wrong'}, 500)
+
+        myvars = dict(receiver=receiver, sender=sender)
+
+        results = db.conn.select('messages', myvars, where='receiver = $receiver or sender = $sender')
+
+        for result in results:
+            print result
 
 
 ##############################################
@@ -50,7 +124,10 @@ class user_new:
 ##############################################
 
 urls = (
-    '/user/new', 'user_new'
+    '/user/new', 'user_new',
+    '/user/get', 'user_get',
+    '/message/new', 'message_new',
+    '/message/get', 'message_get',
 )
 
 def write(payload, status):
